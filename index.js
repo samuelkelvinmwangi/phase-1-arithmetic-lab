39511f6151513742be600db62e69ee259bab07d3
samuel <samuelkelvinmwangi@gmail.com>
2023-03-29 08:30:30 +0300

    arrithmetic assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
 
-// Write your code here
+function multiply(){
+    return multiply;
+}
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+function random(){
+    return random;
+}
+
+let num3 = 8
+let num4 = 4
+let mod = num3 - num4
+
+function mode(){
+    return mod;
+}
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
+
+function max(){
+    return max;
+}
